f496f3eef48497e39e5faeed4105bdc4afac997f
Av <vonnetworking@gmail.com>
2018-07-29 15:01:41 -0400

    working through run_tests.sh issues

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,11 +3,11 @@
 # This file will run unit tests and lint libraries against code in the parent and all
 # subdirectories excluding tests
 
-. ../virtualenv/bin/activate
+. ./virtualenv/bin/activate
 
 rm -f pep8.log pyflakes.log
 
-PYTHONPATH=".." python -m coverage run ..
+PYTHONPATH="." python -m coverage run ..
 
 done
 
@@ -15,4 +15,4 @@ python -m coverage xml -o coverage.xml
 python -m coverage html -d coverage
 
 pep8 --max-line-length=120 ../calc > pep8.log || true
-pylint ../calc > pyflakes.log || true
+pylint calc > pyflakes.log || true
